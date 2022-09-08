b9a94cb08992d51349eb37369bc111d673d179b2
Vitor Recoaro <vitordearec@gmail.com>
2022-09-08 01:30:36 -0300

    CRUD lojas e clientes por rest

--- Atividade_3/src/main/java/Atividade3/domain/Costumer.java
+++ Atividade_3/src/main/java/Atividade3/domain/Costumer.java
@@ -6,6 +6,9 @@ import java.util.List;
 import javax.persistence.*;
 import javax.validation.constraints.*;
 
+import org.json.simple.JSONObject;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 @Entity
 @Table(name = "cliente")
 public class Costumer extends User {
@@ -30,8 +33,8 @@ public class Costumer extends User {
     @Column(nullable = false)
     private Date dataNasc;
 
-    @OneToMany(mappedBy = "usuario")
-    private List<Proposta> listProposta;
+    // @OneToMany(mappedBy = "usuario")
+    // private List<Proposta> listProposta;
 
     public Costumer() {
     }
@@ -75,12 +78,34 @@ public class Costumer extends User {
         this.cpf = usuario.cpf;
     }
 
-    public List<Proposta> getListProposta() {
-        return listProposta;
-    }
+    // public List<Proposta> getListProposta() {
+    // return listProposta;
+    // }
 
-    public void setListProposta(List<Proposta> listProposta) {
-        this.listProposta = listProposta;
+    // public void setListProposta(List<Proposta> listProposta) {
+    // this.listProposta = listProposta;
+    // }
+
+    public void jsonDecode(JSONObject json) {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        Object id = json.get("id");
+
+        if (id != null) {
+            if (id instanceof Integer) {
+                this.setId(((Integer) id).longValue());
+            } else {
+                this.setId((Long) id);
+            }
+        }
+
+        this.setEmail((String) json.get("email"));
+        this.setNome((String) json.get("nome"));
+        this.setSenha(encoder.encode((String) json.get("senha")));
+        this.setTipo((String) json.get("tipo"));
+        this.setCpf((String) json.get("cpf"));
+        this.setTelefone((String) json.get("telefone"));
+        this.setSexo(((String) json.get("sexo")).charAt(0));
+        this.setDataNasc(Date.valueOf((String) json.get("dataNasc")));
     }
 
 }
